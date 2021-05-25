3e48060fc2221b8e341bdb41259975aacc89f70a
Rhevibal-web <82354674+Rhevibal-web@users.noreply.github.com>
2021-05-26 00:10:30 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,466 +1,50 @@
-<html lang="en"><head>
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<meta name="description" content="">
-<meta name="author" content="">
-<title>Graceland</title>
+ <!DOCTYPE html>
+<html>
+<head>
+  <title></title>
+</head>
 
-     <!-- Bootstrap Core CSS -->
- <link href="/Graceland/css/bootstrap.min.css" rel="stylesheet">
+<body style="background-image: url(assets/images/background.jpg); background-repeat: no-repeat;background-size: 100%;background-attachment: fixed;">
+
+ <link href="/Graceland/css2/bootstrap.min.css" rel="stylesheet">
  
     <!-- Custom Fonts -->
     <link href="/Graceland/font/css/font-awesome.min.css" rel="stylesheet" type="text/css">
 
   <link href="/Graceland/font/font-awesome.min.css" rel="stylesheet" type="text/css">
     <!-- DataTables CSS -->
-    <link href="/Graceland/css/dataTables.bootstrap.css" rel="stylesheet">
+    <link href="/Graceland/css2/dataTables.bootstrap.css" rel="stylesheet">
  
      <!-- datetime picker CSS -->
-<link href="/Graceland/css/bootstrap-datetimepicker.min.css" rel="stylesheet" media="screen">
-<link href="/Graceland/css/style.css" rel="stylesheet" media="screen">
+<link href="/Graceland/css2/bootstrap-datetimepicker.min.css" rel="stylesheet" media="screen">
+<link href="/Graceland/css2/style.css" rel="stylesheet" media="screen">
  
-<link href="/Graceland/css/ekko-lightbox.css" rel="stylesheet">
- <link href="/Graceland/css/modern.css" rel="stylesheet">
- <link href="/Graceland/css/costum.css" rel="stylesheet">
- <style type="text/css">
+<link href="/Graceland/css2/ekko-lightbox.css" rel="stylesheet">
+ <link href="/Graceland/css2/modern.css" rel="stylesheet">
+ <link href="/Graceland/css2/costum.css" rel="stylesheet">
+<center>
 
-.p {
-
-  color: white;
-   margin-bottom: 0;
-  margin-top: 0;
-  /*padding: 0;*/
-  /*float: right;*/
-  list-style: none;
-}
-
-.p > a { 
-  color: white;
-  /*text-align: center;*/
-  margin-bottom: 0;
-  margin: 0;
-  padding: 0;
-  text-decoration:none;
-  background-color:  #0000FF;
-}
-.p > a:hover ,
-.p > a:focus {
-   color: maroon; 
-   text-decoration:none;
-   background-color: #2d52f2;
-}
-
-
- 
-.title-logo  {
-    color: maroon;
-    text-decoration:none;
-    font-size: 50px;
-    font-family: "Script MT Bold";
-    /*font-style: bold;*/
-    padding: 0;
-    margin: 0;
-    top: 0;
-  /*  width:1185px;
-    height:337px;*/
-  
-  }
-.title-logo:hover {
-  color: yellow; 
-  text-decoration:none; 
-}
-.title-logo   img {
-  width: 100%;
-  height: 50%;
-}
-.carttxtactive {
-  color: red;
-  font-style: bold;
-  box-shadow: red;
-
-}
-.carttxtactive:hover {
-   color: white;
-}
-
-</style>
-
- 
-<script type="text/javascript">
-   
-
-</script>
-</head>
-
-<body style="background-color:white" onload="totalprice()">
-
-<div class="navbar-fixed-top navbar-TOPsm  col-md-10    col-md-offset-1" role="navigation" style="background-color:maroon;color:maroon">
-  <div class="container">
-    <div class="navbar-header">
-          <h5 class="navbar-menu p">Graceland Online Ordering&lt;</h5>
-         <button type="button" class="navbar-toggle btn-xs p" data-toggle="collapse" data-target=".smMenu">
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span></button> 
-    </div>
-      <div class="collapse navbar-collapse  smMenu "> 
-
-        <ul class="navbar-nav p navbar-left tooltip-demo" style="margin-left:-8%;"> 
-            <li class="dropdown dropdown-toggle ">
-              <a data-toggle="tooltip" data-placement="left" title="" href="/Graceland/contact.php" data-original-title="Contact Us">
-               <i class="fa fa-phone fa-fw"> </i>  09123456789
-              </a>
-            </li>
-            
-          </ul>
-          <ul class="navbar-nav p navbar-right">
-            
-        </ul>
-      </div>
-
-  </div>
-</div>
-
-
- <!-- <div class="col-md-10 col-md-push-1 " style=" margin-top:-2%">  -->
-  <div class="col-md-10 col-md-offset-1 ">  
-   <!-- <div class="col-md-12"> -->
-    <div class="row "> 
-     <p style="margin-top: -50px;" class="title-logo"> 
-        <a class="" href="/Graceland/index.php" title="">
-           <img src="img/banner.jpeg" alt="logo">
-        </a>
-    </p> 
-     </div>    
-   </div>
-
- <div class="navbar navbar-static-top navbar-magbanua col-md-10    col-md-offset-1" role="navigation" style="background-color:red;color:red">
-    
-      <div class="container ">
-        <div class="navbar-header"> 
-            <div class="navbar-menu p">Menu</div>
-          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".bigMenu">
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button> 
-
-       <!--  <a class="navbar-brand"  href="/Graceland/index.php" title="View Sites">GC Appliance Centrum Corp</a> -->
-        </div>
-        <div class="collapse navbar-collapse bigMenu" style="float:left"> 
-          <!-- <ul class="nav navbar-nav" style="margin-left:-4%;"    >  -->
-
-          <ul class="nav navbar-nav"> 
-            <li class="dropdown dropdown-toggle">
-              <a href="/Graceland/index.php"> Home</a>
-            </li>
-            <li class="dropdown-toggle">
-              <a href="/Graceland/menu.php"> Menu</a>
-            </li>
-            <li class="dropdown-toggle">
-              <a href="/Graceland/about.php">About Us</a>
-            </li>
-           <li class="dropdown-toggle">
-             <a href="/Graceland/cart.php"> 
-               Cart <i class="fa fa-shopping-cart fa-fw"> </i> 0             </a>
-          </li>
-          </ul>           
-        </div> 
-    </div> 
-  </div>   
-<div class="col-md-10 col-md-push-1 "> 
-   <!-- start content --> 
-        <div class="row"> 
-          <div id="page-wrapper">
-               
-
-
-            <div class="row">
-              <div class="col-lg-8">
-                <div class="panel panel-default">
-                  <div class="panel-heading">
-                  <b><h4>Menu</h4></b> 
-                  </div>
-                  <div class="panel-body">
-                 
-                     <style type="text/css">
-
- #home{
-  width: 100%;
- }
- #home > tr {
-  width: 50px;
- }
-  .logo {
-    height: 150px;
-    width: 250px;
-    margin-bottom: 5px;
-  }
-
-
-  .logo >  img{
-    height: 100%;  
-    width: 100%;
-
-} 
-.morecontent span {
-    display: none;
-}
-.morelink {
-    display: block;
-}
- </style> 
-  
-    <!-- Projects Row -->
-  <div class="row">
-  <div class="col-md-12">
- 
-  
-
-  <div class="col-lg-6" style="margin-bottom: 5px;">
-
-     <a href=""> 
-    <div class="logo">
-       <img class="img-hover " src="/Graceland/img/product1.jpeg" > 
-    </div>
-    </a> 
-       <span class="more"><br><center><b>
-Tipid Meal    </b> </center> </span>
-  </div>
- 
- 
-  <div class="col-lg-6" style="margin-bottom: 5px;">
-
-     <a href=""> 
-    <div class="logo">
-       <img class="img-hover " src="/Graceland/img/product2.jpeg"> 
-    </div>
-    </a> 
-       <span class="more"><br><center>
-          <b>Noodles Trio</b></center>
-      </span>
-  </div>
- 
- 
-
- 
-    </div>
-</div>  
-
-            
-                     
-                  </div>
-                <!--   <div class="panel-footer">
-                      Panel Footer
-                  </div> -->
-              </div>
-          </div> 
-           <div class="col-lg-4">
-          
-
-
-<!-- login -->
- <div class="panel panel-default">
+ <div style="width: 400px" class="panel panel-default">
     <div class="panel-body">
         <div class="well well-sm"><b><h4>  Login</h4> </b> </div>
 
-            <form class="form-horizontal span6" action="login.php" method="POST">
-                <div class="form-group">
-                  <div class="col-md-12">
-                    <label class="control-label" for="U_USERNAME">Username:</label> 
-                          <input id="U_USERNAME" name="U_USERNAME" placeholder="Username" type="text" class="form-control input">  
-                  </div> 
- 
-                  <div class="col-md-12">
-                    <label class="control-label" for="U_PASS">Password:</label> 
-                     <input name="U_PASS" id="U_PASS" placeholder="Password" type="password" class="form-control input ">
-             
-                  </div> 
-                  </div>
-                  <div class="form-group">
-                  <div class="col-md-12"> 
-                    <button type="submit" id="sidebarLogin" name="sidebarLogin" class="log">  Login</button>  
-                  </div>
-                </div>
- 
-            </form>
+ <form action="includes/processlogin.php" method="post">
+                        <div class="card-body">
+                            <input name="p_username" type="text" class="form-control mb-3" placeholder="Username"><br>
+                            <input name="p_password" type="password" class="form-control mb-3" placeholder="Password">
+                        </div>
+                        <div class="card-footer pb-n1"><br>
+                            <button class="btn btn-primary">Login</button>
+                            <br><a href="createaccount/" class="btn btn-link">Create an Account.</a>
+                        </div>
+                    </form>
+
 
         </div> 
 </div>
               </div>
         </div>
-               </div>
-            <footer class="panel-footer" style="background-color:green;color:white">
-              <p align="center">© 2020 @ BSIT 4A</p>
-           </footer>
-      </div>
-
-  </div>  
-<!-- end of page  -->
-
-
- <!-- modalorder -->
- <div class="modal fade" id="myOrdered">
- </div>
-<!-- end -->
- 
-    <!-- jQuery -->
-    <script src="/Graceland/jquery/jquery.min.js"></script>
-
-    <!-- Bootstrap Core JavaScript -->
-    <script src="/Graceland/js/bootstrap.min.js"></script>
-
-    <!-- Metis Menu Plugin JavaScript --> 
-    <!-- DataTables JavaScript -->
-    <script src="/Graceland/js/jquery.dataTables.min.js"></script>
-    <script src="/Graceland/js/dataTables.bootstrap.min.js"></script>
-
-
-<script type="text/javascript" language="javascript" src="/Graceland/js/ekko-lightbox.js"></script> 
-<script type="text/javascript" src="/Graceland/js/bootstrap-datetimepicker.js" charset="UTF-8"></script>
-<script type="text/javascript" src="/Graceland/js/locales/bootstrap-datetimepicker.uk.js" charset="UTF-8"></script>
-
-    <!-- Custom Theme JavaScript --> 
-<script type="text/javascript" language="javascript" src="/Graceland/js/janobe.js"></script> 
- <script type="text/javascript">
-  $(document).on("click", ".proid", function () {
-    // var id = $(this).attr('id');
-      var proid = $(this).data('id')
-    // alert(proid)
-       $(".modal-body #proid").val( proid )
-
-      });
-
-</script>
- <script>
-    // tooltip demo
-    $('.tooltip-demo').tooltip({
-        selector: "[data-toggle=tooltip]",
-        container: "body"
-    })
-
-    // popover demo
-    $("[data-toggle=popover]")
-        .popover()
-    </script>
-
-
-      <script>
-        $('.carousel').carousel({
-            interval: 5000 //changes the speed
-        })
-    </script>
-
-<script type="text/javascript">
-
-
-$('#date_picker').datetimepicker({
-  format: 'mm/dd/yyyy',
-    language:  'en',
-    weekStart: 1,
-    todayBtn:  1,
-    autoclose: 1,
-    todayHighlight: 1,
-    startView: 2,
-    minView: 2,
-    forceParse: 0
-    });
-
- 
- 
- 
-function validatedate(){ 
- 
- 
-
-    var todaysDate = new Date() ;
-
-    var txtime =  document.getElementById('ftime').value
-    // var myDate = new Date(dateme); 
-
-    var tprice = document.getElementById('alltot').value 
-    var BRGY = document.getElementById('BRGY').value
-    var onum = document.getElementById('ORDERNUMBER').value
-
-     
-     var mytime = parseInt(txtime)  ;
-     var todaytime =  todaysDate.getHours()  ;
-       if (txtime==""){
-     alert("You must set the time enable to submit the order.")
-     }else 
-     if (mytime<todaytime){ 
-        alert("Selected time is invalid. Set another time.")
-      }else{
-        window.location = "index.php?page=7&price="+tprice+"&time="+txtime+"&BRGY="+BRGY+"&ordernumber="+onum; 
-      }
-  }
-</script>  
-
-
-    <script type="text/javascript">
-  $('.form_curdate').datetimepicker({
-        language:  'en',
-        weekStart: 1,
-        todayBtn:  1,
-        autoclose: 1,
-        todayHighlight: 1,
-        startView: 2,
-        minView: 2,
-        forceParse: 0
-    });
-  $('.form_bdatess').datetimepicker({
-        language:  'en',
-        weekStart: 1,
-        todayBtn:  1,
-        autoclose: 1,
-        todayHighlight: 1,
-        startView: 2,
-        minView: 2,
-        forceParse: 0
-    });
-</script>
-<script>
- 
-
-
-  function checkall(selector)
-  {
-    if(document.getElementById('chkall').checked==true)
-    {
-      var chkelement=document.getElementsByName(selector);
-      for(var i=0;i<chkelement.length;i++)
-      {
-        chkelement.item(i).checked=true;
-      }
-    }
-    else
-    {
-      var chkelement=document.getElementsByName(selector);
-      for(var i=0;i<chkelement.length;i++)
-      {
-        chkelement.item(i).checked=false;
-      }
-    }
-  }
-   function checkNumber(textBox){
-        while (textBox.value.length > 0 && isNaN(textBox.value)) {
-          textBox.value = textBox.value.substring(0, textBox.value.length - 1)
-        }
-        textBox.value = trim(textBox.value);
-      }
-      //
-      function checkText(textBox)
-      {
-        var alphaExp = /^[a-zA-Z]+$/;
-        while (textBox.value.length > 0 && !textBox.value.match(alphaExp)) {
-          textBox.value = textBox.value.substring(0, textBox.value.length - 1)
-        }
-        textBox.value = trim(textBox.value);
-      }
-  
-
        
-  </script>     
 
-
-
-</body></html>
+</body>
+</html>
